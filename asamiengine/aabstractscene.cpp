3087933c4e9240876538b53b0b99c44b25aa974e
vduseev <vagiz.d@gmail.com>
2017-05-30 14:19:48 +0200

    Changed formatting of tabs in source

--- asamiengine/aabstractscene.cpp
+++ asamiengine/aabstractscene.cpp
@@ -11,8 +11,8 @@
 #include <qdebug.h>
 
 AAbstractScene::AAbstractScene( QObject* parent )
-	: QObject( parent )
-	, m_time( 0 )
+    : QObject( parent )
+    , m_time( 0 )
 {
 }
 
@@ -22,40 +22,40 @@ AAbstractScene::~AAbstractScene(void)
 
 void AAbstractScene::update( float time )
 {
-	float dt = time - m_time;
-	m_time = time;
+    float dt = time - m_time;
+    m_time = time;
 
-	// initialize identity matrix
-	QMatrix4x4 parent, parentRotation;
+    // initialize identity matrix
+    QMatrix4x4 parent, parentRotation;
 
-	// recompute all nodes
-	for ( int i = 0; i < m_nodes.count(); i++ )
-	{
-		ANode* node = m_nodes.at( i );		
-		node->compute( parent );
-		node->computeRotation( parentRotation );
-		parent = node->computedMatrix();
-		parentRotation = node->computedRotationMatrix();
-	}
+    // recompute all nodes
+    for ( int i = 0; i < m_nodes.count(); i++ )
+    {
+        ANode* node = m_nodes.at( i );
+        node->compute( parent );
+        node->computeRotation( parentRotation );
+        parent = node->computedMatrix();
+        parentRotation = node->computedRotationMatrix();
+    }
 
-	// update objects
-	for ( int i = 0; i < m_objects.count(); i++ )
-	{
-		AObject3D* object = m_objects.at( i );
-		object->update( time );
-	}
+    // update objects
+    for ( int i = 0; i < m_objects.count(); i++ )
+    {
+        AObject3D* object = m_objects.at( i );
+        object->update( time );
+    }
 
-	float fps = 1.0f / dt;
-	qDebug() << "Frames per second: " << fps;
+    float fps = 1.0f / dt;
+    qDebug() << "Frames per second: " << fps;
 }
 
 void AAbstractScene::render()
 {
-	for ( int i = 0; i < m_objects.count(); i++ )
-	{
-		AObject3D* object = m_objects.at( i );
-		object->draw( m_projectionMatrix, m_viewMatrix, m_cameraPosition, m_lights );
-	}	
+    for ( int i = 0; i < m_objects.count(); i++ )
+    {
+        AObject3D* object = m_objects.at( i );
+        object->draw( m_projectionMatrix, m_viewMatrix, m_cameraPosition, m_lights );
+    }
 }
 
 /*-------------------------------
@@ -64,17 +64,17 @@ void AAbstractScene::render()
 
 void AAbstractScene::addNode( ANode* node )
 {
-	m_nodes.append( node );
+    m_nodes.append( node );
 }
 
 void AAbstractScene::addObject3D( AObject3D* object )
 {
-	m_objects.append( object );
+    m_objects.append( object );
 }
 
 void AAbstractScene::addLight3D( AAbstractLight* light )
 {
-	m_lights.append( light );
+    m_lights.append( light );
 }
 
 /*-----------------------------------------------
