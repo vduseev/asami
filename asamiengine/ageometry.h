ae61bd4560aba4dddda0ae2c159cdfaa9430634c
vduseev <vagiz.d@gmail.com>
2017-06-03 14:26:20 +0200

    Changed formatting in geometry header

--- asamiengine/ageometry.h
+++ asamiengine/ageometry.h
@@ -11,43 +11,43 @@ class ASimpleTextureVertex;
 class AGeometry
 {
 public:
-	AGeometry();
-	~AGeometry();
+    AGeometry();
+    ~AGeometry();
 
-	void setUsagePattern( QOpenGLBuffer::UsagePattern pattern, QOpenGLBuffer::Type bufferType );
+    void setUsagePattern( QOpenGLBuffer::UsagePattern pattern, QOpenGLBuffer::Type bufferType );
 
-	void setVertices( const void* data, int count, int size, int offsetElements = 0 );
-	void setIndices( GLuint* indices, int count, int offsetElements = 0 );
-	void setVertexAttribute( const QByteArray& name, int offset, int tupleSize, int stride = 0 );
+    void setVertices( const void* data, int count, int size, int offsetElements = 0 );
+    void setIndices( GLuint* indices, int count, int offsetElements = 0 );
+    void setVertexAttribute( const QByteArray& name, int offset, int tupleSize, int stride = 0 );
 
-	void setVetexBuffer( QOpenGLBuffer* vertexBuffer ) { m_vertexBuffer = vertexBuffer; }
-	QOpenGLBuffer* vertexBuffer() { return m_vertexBuffer; }
+    void setVetexBuffer( QOpenGLBuffer* vertexBuffer ) { m_vertexBuffer = vertexBuffer; }
+    QOpenGLBuffer* vertexBuffer() { return m_vertexBuffer; }
 
-	void setIndexBuffer( QOpenGLBuffer* indexBuffer ) { m_indexBuffer = indexBuffer; }
-	QOpenGLBuffer* indexBuffer() { return m_indexBuffer; }
+    void setIndexBuffer( QOpenGLBuffer* indexBuffer ) { m_indexBuffer = indexBuffer; }
+    QOpenGLBuffer* indexBuffer() { return m_indexBuffer; }
 
-	void setVertexAttribute( AVertexAttribute* vertexAttribute );
-	QList<AVertexAttribute*> vertexAttributes() { return m_vertexAttributes; }
+    void setVertexAttribute( AVertexAttribute* vertexAttribute );
+    QList<AVertexAttribute*> vertexAttributes() { return m_vertexAttributes; }
 
-	int vertexCount() const { return m_vertexCount; }
-	int indexCount() const { return m_indexCount; }
+    int vertexCount() const { return m_vertexCount; }
+    int indexCount() const { return m_indexCount; }
 
-	static void calculateNormals( 
-		ASimpleTextureVertex* vertices, int vertexCount, 
-		GLuint* indices, int indexCount );
+    static void calculateNormals(
+        ASimpleTextureVertex* vertices, int vertexCount,
+        GLuint* indices, int indexCount
+    );
 
 private:
-	QOpenGLBuffer* m_vertexBuffer;
-	QOpenGLBuffer* m_indexBuffer;
+    QOpenGLBuffer* m_vertexBuffer;
+    QOpenGLBuffer* m_indexBuffer;
 
-	QList<AVertexAttribute*> m_vertexAttributes;
+    QList<AVertexAttribute*> m_vertexAttributes;
 
-	int m_vertexBufferAllocatedCount;
-	int m_indexBufferAllocatedCount;
-	
-	int m_vertexCount;
-	int m_indexCount;
+    int m_vertexBufferAllocatedCount;
+    int m_indexBufferAllocatedCount;
+
+    int m_vertexCount;
+    int m_indexCount;
 };
 
 #endif
-
