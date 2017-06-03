2242f33e90214cc1933c5fe9be1070f16d14d6f8
vduseev <vagiz.d@gmail.com>
2017-06-03 14:25:56 +0200

    Changed formatting in header

--- asamiengine/adirectionallight.h
+++ asamiengine/adirectionallight.h
@@ -6,14 +6,14 @@
 class ADirectionalLight : public AAbstractLight
 {
 public:
-	ADirectionalLight();
-	~ADirectionalLight();
+    ADirectionalLight();
+    ~ADirectionalLight();
 
-	void setDirection( QVector3D direction );
-	QVector3D direction() const { return m_direction; }
+    void setDirection( QVector3D direction );
+    QVector3D direction() const { return m_direction; }
 
 protected:
-	QVector3D m_direction;
+    QVector3D m_direction;
 };
 
 #endif
