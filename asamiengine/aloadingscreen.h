6f4e1b91a393aadfdbe6cb156e9d067fa9f60798
vduseev <vagiz.d@gmail.com>
2017-06-05 14:27:59 +0200

    Changed loading screen header formatting

--- asamiengine/aloadingscreen.h
+++ asamiengine/aloadingscreen.h
@@ -9,27 +9,26 @@ class ANode;
 class ALoadingScreen : public AAbstractScene
 {
 public:
-	ALoadingScreen();
-	~ALoadingScreen();
+    ALoadingScreen();
+    ~ALoadingScreen();
 
-	void initialize();
-	void update( float time );
-	void render();
-	void resize( int width, int height );
+    void initialize();
+    void update( float time );
+    void render();
+    void resize( int width, int height );
 
 private:
-	float m_time;
+    float m_time;
 
-	ANode* m_node;
+    ANode* m_node;
 
-	ACamera*	m_camera;
-    QVector3D	m_v;
-    bool	m_viewCenterFixed;
-    float	m_panAngle;
-    float	m_tiltAngle;
+    ACamera*    m_camera;
+    QVector3D   m_v;
+    bool        m_viewCenterFixed;
+    float       m_panAngle;
+    float       m_tiltAngle;
     const float m_metersToUnits;
-	const float m_cameraSpeed;
+    const float m_cameraSpeed;
 };
 
 #endif
-
