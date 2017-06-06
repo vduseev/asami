6e12027002d844f7a546afbfce451e9a69ff621a
vduseev <vagiz.d@gmail.com>
2017-06-06 14:28:17 +0200

    Changed loading screen source formatting

--- asamiengine/aloadingscreen.cpp
+++ asamiengine/aloadingscreen.cpp
@@ -8,8 +8,8 @@
 #include <QList>
 
 ALoadingScreen::ALoadingScreen()
-	: m_metersToUnits( 0.05f )
-	, m_cameraSpeed( 300.0f )
+    : m_metersToUnits( 0.05f )
+    , m_cameraSpeed( 300.0f )
 {
 }
 
@@ -18,52 +18,52 @@ ALoadingScreen::~ALoadingScreen(void)
 }
 
 void ALoadingScreen::initialize()
-{		
-	m_camera = new ACamera( this );
-	m_v = QVector3D();
+{
+    m_camera = new ACamera( this );
+    m_v = QVector3D();
     m_viewCenterFixed = false;
     m_panAngle = 0.0f;
     m_tiltAngle = 0.0f;
 
-	// Initialize the camera position and orientation
+    // Initialize the camera position and orientation
     m_camera->setPosition( QVector3D( 0.0f, 0.0f, 10.0f ) );
     m_camera->setViewCenter( QVector3D( 0.0f, 0.0f, 0.0f ) );
     m_camera->setUpVector( QVector3D( 0.0f, 1.0f, 0.0f ) );
 
-	ATexturedQuad* testquadFront = new ATexturedQuad( "textures/asami/asami_electro.png" );
-	AAbstractScene::addObject3D( testquadFront );
+    ATexturedQuad* testquadFront = new ATexturedQuad( "textures/asami/asami_electro.png" );
+    AAbstractScene::addObject3D( testquadFront );
 
-	ATexturedQuad* testquadBack = new ATexturedQuad( "textures/asami/black.png" );
-	testquadBack->rotate( 180.0f, 0, 1, 0 );
-	AAbstractScene::addObject3D( testquadBack );
+    ATexturedQuad* testquadBack = new ATexturedQuad( "textures/asami/black.png" );
+    testquadBack->rotate( 180.0f, 0, 1, 0 );
+    AAbstractScene::addObject3D( testquadBack );
 
-	m_node = new ANode();
-	AAbstractScene::addNode( m_node );
+    m_node = new ANode();
+    AAbstractScene::addNode( m_node );
 
-	testquadFront->associate( m_node );
-	testquadBack->associate( m_node );
+    testquadFront->associate( m_node );
+    testquadBack->associate( m_node );
 }
 
 void ALoadingScreen::update( float time )
 {
-	const float dt = time - m_time;
-	m_time = time;
+    const float dt = time - m_time;
+    m_time = time;
 
-	m_node->rotate( dt * 50, 0, 1, 0 );
+    m_node->rotate( dt * 50, 0, 1, 0 );
 
-	AAbstractScene::setCameraPosition( m_camera->position() );
-	AAbstractScene::setCameraViewMatrix( m_camera->viewMatrix() );
-	AAbstractScene::setCameraProjectionMatrix( m_camera->projectionMatrix() );
-	AAbstractScene::update( time );
+    AAbstractScene::setCameraPosition( m_camera->position() );
+    AAbstractScene::setCameraViewMatrix( m_camera->viewMatrix() );
+    AAbstractScene::setCameraProjectionMatrix( m_camera->projectionMatrix() );
+    AAbstractScene::update( time );
 }
 
 void ALoadingScreen::render()
 {
-	AAbstractScene::render();
+    AAbstractScene::render();
 }
 
 void ALoadingScreen::resize( int width, int height )
 {
-	float aspect = static_cast<float>( width ) / static_cast<float>( height );
+    float aspect = static_cast<float>( width ) / static_cast<float>( height );
     m_camera->setPerspectiveProjection( 45.0f, aspect, 0.01f, 3000.0f );
 }
