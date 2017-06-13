bafcc050fff763b9fca973122d3d9ecb3b78f27b
vduseev <vagiz.d@gmail.com>
2017-06-13 14:36:22 +0200

    Fixed hard tabs in header

--- asamiengine/aopenglwindow.h
+++ asamiengine/aopenglwindow.h
@@ -16,40 +16,40 @@ class AOpenGLWindow : public QWindow, protected QOpenGLFunctions_4_0_Core
 public:
     explicit AOpenGLWindow(QWindow *parent = 0);
     ~AOpenGLWindow();
-	
+
 public slots:
 
-	// Called every frame by timer
-	void tick();
-	// Called when window is resized
-	void resizeEngine();
+    // Called every frame by timer
+    void tick();
+    // Called when window is resized
+    void resizeEngine();
 
-protected:	
+protected:
     void keyPressEvent( QKeyEvent* e );
     void keyReleaseEvent( QKeyEvent* e );
     void mousePressEvent( QMouseEvent* e );
     void mouseReleaseEvent( QMouseEvent* e );
     void mouseMoveEvent( QMouseEvent* e );
-	
+
 private:
-	void initializeEngine();
-    void renderEngine();	
-	void updateEngine();
+    void initializeEngine();
+    void renderEngine();
+    void updateEngine();
 
     QOpenGLContext *m_context;
-	bool m_isEngineInitialized;
+    bool m_isEngineInitialized;
 
-	bool m_leftButtonPressed;
-	bool m_isFullscreen;
+    bool m_leftButtonPressed;
+    bool m_isFullscreen;
     QPoint m_prevPos;
     QPoint m_pos;
-	QTime m_time;	
+    QTime m_time;
     int m_frame;
-	float m_refreshRate;
+    float m_refreshRate;
 
-	AAbstractScene* m_engineScene;
-	AAbstractScene* m_loadingScene;
-	AAbstractScene* m_currentScene;
+    AAbstractScene* m_engineScene;
+    AAbstractScene* m_loadingScene;
+    AAbstractScene* m_currentScene;
 };
 
 #endif
