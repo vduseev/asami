28787796b19bd20f2739c43d58d660724c6a7c2d
vduseev <vagiz.d@gmail.com>
2017-06-23 14:41:45 +0200

    Replaced hard tabs

--- asamiengine/atestscene.h
+++ asamiengine/atestscene.h
@@ -13,53 +13,52 @@ class ANode;
 
 class ATestScene : public AAbstractScene
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	ATestScene();
-	~ATestScene();
+    ATestScene();
+    ~ATestScene();
 
-	void initialize();
-	void update( float time );
-	void render();
-	void resize( int width, int height );
+    void initialize();
+    void update( float time );
+    void render();
+    void resize( int width, int height );
 
-	// Camera motion control
-	void setSideSpeed( float dir )		{ m_v.setX( dir * m_cameraSpeed ); }
-    void setVerticalSpeed( float dir )	{ m_v.setY( dir * m_cameraSpeed ); }
-    void setForwardSpeed( float dir )	{ m_v.setZ( dir * m_cameraSpeed ); }
-    void setViewCenterFixed( bool b )	{ m_viewCenterFixed = b; }
+    // Camera motion control
+    void setSideSpeed( float dir )        { m_v.setX( dir * m_cameraSpeed ); }
+    void setVerticalSpeed( float dir )    { m_v.setY( dir * m_cameraSpeed ); }
+    void setForwardSpeed( float dir )    { m_v.setZ( dir * m_cameraSpeed ); }
+    void setViewCenterFixed( bool b )    { m_viewCenterFixed = b; }
 
     // Camera orientation control
     void pan( float angle ) { m_panAngle = angle; }
     void tilt( float angle ) { m_tiltAngle = angle; }
 
-	void keyPressEvent( Qt::Key key );
+    void keyPressEvent( Qt::Key key );
     void keyReleaseEvent( Qt::Key key );
     void mousePressEvent( QMouseEvent* e );
     void mouseReleaseEvent( QMouseEvent* e );
     void mouseMoveEvent( QMouseEvent* e );
 
-	float m( float distance );
+    float m( float distance );
 
 private:
-	ANode* m_asamiNode;
-	AObject3D* m_banner;
-	AObject3D* m_bannerBack;
+    ANode* m_asamiNode;
+    AObject3D* m_banner;
+    AObject3D* m_bannerBack;
 
-	ACamera*	m_camera;
-    QVector3D	m_v;
-    bool	m_viewCenterFixed;
-    float	m_panAngle;
-    float	m_tiltAngle;
-	float	m_cameraSpeed;
+    ACamera*    m_camera;
+    QVector3D    m_v;
+    bool    m_viewCenterFixed;
+    float    m_panAngle;
+    float    m_tiltAngle;
+    float    m_cameraSpeed;
 
     QMatrix4x4 m_viewportMatrix;
     QVector2D m_viewportSize;
 
-	float m_time;	
+    float m_time;
     const float m_metersToUnits;
 };
 
 #endif
-
