2fb7e883bc14be6005b957ab942fd91262e1fcd7
vduseev <vagiz.d@gmail.com>
2017-06-24 14:42:30 +0200

    Fixed identation after hard tabs replacement

--- asamiengine/atestscene.h
+++ asamiengine/atestscene.h
@@ -25,10 +25,10 @@ public:
     void resize( int width, int height );
 
     // Camera motion control
-    void setSideSpeed( float dir )        { m_v.setX( dir * m_cameraSpeed ); }
-    void setVerticalSpeed( float dir )    { m_v.setY( dir * m_cameraSpeed ); }
-    void setForwardSpeed( float dir )    { m_v.setZ( dir * m_cameraSpeed ); }
-    void setViewCenterFixed( bool b )    { m_viewCenterFixed = b; }
+    void setSideSpeed( float dir )      { m_v.setX( dir * m_cameraSpeed ); }
+    void setVerticalSpeed( float dir )  { m_v.setY( dir * m_cameraSpeed ); }
+    void setForwardSpeed( float dir )   { m_v.setZ( dir * m_cameraSpeed ); }
+    void setViewCenterFixed( bool b )   { m_viewCenterFixed = b; }
 
     // Camera orientation control
     void pan( float angle ) { m_panAngle = angle; }
@@ -43,19 +43,19 @@ public:
     float m( float distance );
 
 private:
-    ANode* m_asamiNode;
-    AObject3D* m_banner;
-    AObject3D* m_bannerBack;
+    ANode*      m_asamiNode;
+    AObject3D*  m_banner;
+    AObject3D*  m_bannerBack;
 
     ACamera*    m_camera;
-    QVector3D    m_v;
-    bool    m_viewCenterFixed;
-    float    m_panAngle;
-    float    m_tiltAngle;
-    float    m_cameraSpeed;
+    QVector3D   m_v;
+    bool        m_viewCenterFixed;
+    float       m_panAngle;
+    float       m_tiltAngle;
+    float       m_cameraSpeed;
 
-    QMatrix4x4 m_viewportMatrix;
-    QVector2D m_viewportSize;
+    QMatrix4x4  m_viewportMatrix;
+    QVector2D   m_viewportSize;
 
     float m_time;
     const float m_metersToUnits;
