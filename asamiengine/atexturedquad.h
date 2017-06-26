bd5609050bb24d642845701d7f3d2f94dae8bd43
vduseev <vagiz.d@gmail.com>
2017-06-26 14:43:41 +0200

    Replaced hard tabs

--- asamiengine/atexturedquad.h
+++ asamiengine/atexturedquad.h
@@ -10,32 +10,31 @@
 class ATexturedQuad : public AObject3D
 {
 public:
-	ATexturedQuad( QSize textureCoordinates = QSize( 1, 1 ) );
-	ATexturedQuad(
-		const QString& textureFileName,
-		QSize textureCoordinates = QSize( 1, 1 ) );
-	ATexturedQuad(
-		const QString& textureFileName,
-		const QString& vertexShader, 
-		const QString& fragmentShader,
-		QSize textureCoordinates = QSize( 1, 1 ) );
-	~ATexturedQuad();
+    ATexturedQuad( QSize textureCoordinates = QSize( 1, 1 ) );
+    ATexturedQuad(
+        const QString& textureFileName,
+        QSize textureCoordinates = QSize( 1, 1 ) );
+    ATexturedQuad(
+        const QString& textureFileName,
+        const QString& vertexShader,
+        const QString& fragmentShader,
+        QSize textureCoordinates = QSize( 1, 1 ) );
+    ~ATexturedQuad();
 
-	class Vertex
-	{
-	public:
-		QVector3D pos;
-		QVector3D norm;
-		QVector2D tex;
-	};
+    class Vertex
+    {
+    public:
+        QVector3D pos;
+        QVector3D norm;
+        QVector2D tex;
+    };
 
 private:
-	void prepareGeometry( QSize textureCoordinates );
-	void prepareMaterial( 
-		const QString& textureFileName,
-		const QString& vertexShader, 
-		const QString& fragmentShader );
+    void prepareGeometry( QSize textureCoordinates );
+    void prepareMaterial(
+        const QString& textureFileName,
+        const QString& vertexShader,
+        const QString& fragmentShader );
 };
 
 #endif
-
